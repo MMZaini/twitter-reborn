312e461fbea7f994cac19f557c998821a5460b06
MMZaini <mahdizainipro@gmail.com>
2024-11-10 07:37:42 +0000

    update secrets

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,7 +1,11 @@
 // supabaseClient.ts
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zexlxfjlbfesbwsadlob.supabase.co'; // replace with your Supabase URL
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpleGx4ZmpsYmZlc2J3c2FkbG9iIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEyMDA2NDgsImV4cCI6MjA0Njc3NjY0OH0.lcHz-8G1fwkcfH4topa8ZL8l2wM0ZrFbtirQdpuLLaU'; // replace with your anon key
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+  throw new Error('Missing Supabase environment variables');
+}
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
